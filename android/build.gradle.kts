37448e8b3427636b1b5f0e13449681943d7084d9
Ned Wolpert <ned.wolpert@gmail.com>
2026-05-05 18:07:52 -0700

    Proguard fix

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -48,7 +48,7 @@ android {
     buildTypes {
         getByName("release") {
             isMinifyEnabled = true
-            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
 }
